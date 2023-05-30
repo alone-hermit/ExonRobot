5746314d2eb47793eadfbb9fe7883c976e8f2bf5
alone-hermit <133490910+alone-hermit@users.noreply.github.com>
2023-05-30 20:45:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
     OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001871477087"))
     MONGO_URI = getenv("MONGO_DB_URI")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL")
